fe7d790d1c0f0bd18124566e7b8038969cc42bf0
Koushik Sinha <38983102+koussiksinha@users.noreply.github.com>
2018-06-11 18:14:22 +0530

    Update Table_res.sql

--- Table_res.sql
+++ Table_res.sql
@@ -15,21 +15,37 @@ CREATE TABLE IF NOT EXISTS `res` (
   `src` varchar(100) NOT NULL,
   `des` varchar(100) NOT NULL,
   `date` date NOT NULL,
+  -- Total No of tickets available.
   `AC_I` int(6) NOT NULL,
+  -- No of tickets booked.
   `book` int(6) NOT NULL,
+  --bal of tickes after every reservation.
   `bal` int(6) NOT NULL,
+  --Fare of this coach.
   `FIRST_AC` int(6) NOT NULL,
+  -- Total No of tickets available.
   `AC_II` int(6) NOT NULL,
+  -- No of tickets booked.
   `book1` int(6) NOT NULL,
+  --bal of tickes after every reservation.
   `bal1` int(6) NOT NULL,
+  --Fare of this coach.
   `SEC_AC` int(6) NOT NULL,
+  --Total no. of tickets available.
   `AC_III` int(6) NOT NULL,
+   --No. of tickets booked.
   `book2` int(6) NOT NULL,
+  --balance tickes after booking.
   `bal2` int(6) NOT NULL,
+  --Fare of this coach.
   `THIRD_AC` int(6) NOT NULL,
+  -- Total no. of tickets available.
   `SL` int(6) NOT NULL,
+  --No. of tickets booked.
   `book3` int(6) NOT NULL,
+  --balance tickes after booking.
   `bal3` int(6) NOT NULL,
+      --Fare of this coach.
   `SL_CLASS` int(6) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `TRAIN_NUMBER` (`TRAIN_NUMBER`,`Tr_name`)
