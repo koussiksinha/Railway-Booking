46167408b2f6c8c998151761b9cbd37f8d7e0644
Koushik Sinha <38983102+koussiksinha@users.noreply.github.com>
2018-06-12 02:22:05 +0530

    Update userdt2.php

--- userdt2.php
+++ userdt2.php
@@ -11,6 +11,19 @@ echo "<div class='Top'>";
 						echo "<button><a href='userdt.php'>Previous Page</a></button><br /><br />";
  echo "</div> <br/>";
  $cx=$_SESSION['id'];
+ echo "<br/>
+<div align='center'>
+<marquee 
+direction='left'
+loop='15'
+scrollamount='2'
+     scrolldelay='2'
+     behavior='alternate'
+     width='70%'
+     	 >
+<font bgcolor='white'>Welcome To&nbsp;&nbsp;Admin Page</font>
+</marquee>
+</div>";
 echo "<div style='position: absolute; top: 0; right: 0px; width: -40px; text-align:right;'>
 
 			<font color='Crimson' size='4'>
@@ -19,13 +32,14 @@ echo "<div style='position: absolute; top: 0; right: 0px; width: -40px; text-ali
 
 						<u>Click here to</u>&nbsp;&nbsp;&nbsp;<a href='logout1.php' title='Logout'>Logout</a></font></div>";
 echo "<h2 style='text-align:center'><u>DELETE USER</u></h2>";
-$upp=$_GET['id'];
-$l=$_POST['id'];
-$b=$_POST['uname'];
-$c=$_POST['fname'];
-$d=$_POST['lname'];
-$e=$_POST['email'];
-$f=$_POST['password'];
+		
+@$upp=$_GET['id'];
+@$l=$_POST['id'];
+@$b=$_POST['uname'];
+@$c=$_POST['fname'];
+@$d=$_POST['lname'];
+@$e=$_POST['email'];
+@$f=$_POST['password'];
 
 $s="select * from test where id='$upp'";
 $run=mysqli_query($con,$s);
@@ -36,7 +50,8 @@ $b=$row['uname'];
    $d=$row['lname'];
    $e=$row['email'];
    $f=$row['password'];
-     echo "<table style='width:100%' border='1' 'rowspan='10'  bgcolor='#999999'>
+   
+  echo "<table style='width:100%' border='1' 'rowspan='10'  bgcolor='#999999'>
 <tr>";
 			    echo "<th>ID</th>
 				<th>USER_NAME</th>
@@ -60,6 +75,8 @@ echo "<tr>";
 	  echo "<th width='9%'>"."<input type='password' style=background-color='#999999;' value='$x'>"."</th>";
 echo "</tr>";
 	  	}
+
+	  
 echo "</table>";  
 ?>
 <!DOCTYPE html>
@@ -67,6 +84,7 @@ echo "</table>";
 <head>
 <style>
 body {color: purple;background-color:#FA8072; }
+
 h1{
 	color:white ;
 	font- size :25px;
@@ -77,41 +95,51 @@ h1{
 </head>
 <body>
 <br><br/>
+
 <tr>
 <td></td>
 <td colspan="2" rowspan="2"></td>
 </tr>
 <table CELLPADDING="4" cellspacing="10" background="TEAL"></table>
 <form name="form" action="userdt1.php?=$upp" method="POST" >
+
 <table align="left" width="20%" border="0"></table>
+
 <div class="header">
 <th colspan="2"><h2 align="center" ><strong><u>DELETE USER</u></strong></h2>
 </div>
 <fieldset>
+
 <p>
 <font color="PURPLE"><label for="User Name"><u>ID</u></label></font>
 <input type="Text" name="id" style="background-color:#999999; color:white;"  value="<?php echo $l;?>" readonly>
 </p>
+
 <p>
 <font color="PURPLE"><label for="User Name"><u>User Name</u></label></font>
 <input type="Text" name="uname" style="background-color:#999999; color:white;" value="<?php echo $b;?>" readonly>
 </p>
+
 <p>
 <font color="PURPLE"><label for="First Name"><u>First name </u></label></font>
 <input type="Text" name="fname"  style="background-color:#999999; color:white;" value="<?php echo $c;?>">
 </p>
+
 <p>
 <font color="PURPLE"><label for="Last Name"><u> Last Name</u></label></font>
 <input type="Text" name="lname" style="background-color:#999999; color:white;" value="<?php echo $d;?>">
 </p>
+
 <p>
 <font color="PURPLE"><label for="email"><u>Email</u></label></font>
 <input type="email" name="email" style="background-color:#999999; color:white;" value="<?php echo $e;?>" readonly>
 </p>
+
 <p>
 <font color="PURPLE"><label for="password"><u>Password</u></label></font> 
 <input type="password" name="password" style="background-color:#999999; color:white;" value="<?php echo $f;?>" readonly >
 </p>
+
 <td><td><input type="submit" name="submit" value="DELETE"></td></td>
 
 </fieldset>
@@ -120,18 +148,19 @@ h1{
 </html>
 <?php
 include 'connect1.php';
-	if(isset($_POST['submit'])){
-			$c=$_POST['fname'];
-				$d=$_POST['lname'];
-	$sql= "DELETE from test where id='$l'";
-				$d=mysqli_query($con,$sql);
-		if($d){
-	
-    			echo "Records were deleted successfully";
-				echo "<div align='center'><font size='6'>Redirecting in 2 secs.....</font></div>";
-					header("refresh:2; url=userdt.php");	
+if(isset($_POST['submit'])){
+@$c=$_POST['fname'];
+@$d=$_POST['lname'];
 
-			}
+$sql= "DELETE from test where id='$l'";
+$d=mysqli_query($con,$sql);
+if($d){
+	
+    echo "Records were deleted successfully";
+echo "<div align='center'><font size='6'>Redirecting in 2 secs.....</font></div>";
+	header("refresh:2; url=userdt.php");	
+
+}
  else {
     echo "ERROR: Could not able to execute";
 echo "<div align='center'><font size='6'>Redirecting you back.....</font></div>";
