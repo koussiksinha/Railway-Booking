c85bc4aeeb21d5cdca655181bea8cabdb14c6ad6
Koushik Sinha <38983102+koussiksinha@users.noreply.github.com>
2018-06-12 03:01:14 +0530

    Update res2.php

--- res2.php
+++ res2.php
@@ -19,7 +19,19 @@ body {
 session_start(); 
 ?>
 <?php
-echo"<br>";
+echo"<br>
+<div align='center'>
+<marquee 
+direction='left'
+loop='15'
+scrollamount='2'
+     scrolldelay='2'
+     behavior='alternate'
+     width='70%'
+     	 >
+<font bgcolor='white'>Welcome To&nbsp;&nbsp;E-Ticketing Website</font>
+</marquee>
+</div>";
 $b="";
 $_SESSION['fname']=$b;$cx=$_SESSION['id'];
 echo "<font color='purple'>";
@@ -27,14 +39,21 @@ include 'timedate.php';
 echo "</font>";
 include 'connect1.php';
 
-			echo "<div align='right'><font color='#86af49' size='4'><u>Welcome"." ".$_SESSION['id']." ".""." "." "."<br/><br/></div>
-							<div align='right'><a href='logout.php' title='Logout'><font color='#86af49' size='4'>Logout</font></a></div>";
+			echo "<div style='position: absolute; top: 0; right: 0px; width: -40px; text-align:right;'>
+
+			<font color='Crimson' size='4'>
+
+				<u>Welcome Admin</u>"."&nbsp;"."&nbsp;"."&nbsp;".$_SESSION['id']."<br><br>
+
+						<u>Click here to</u>&nbsp;&nbsp;&nbsp;<a href='logout1.php' title='Logout'>Logout</a></font></div>";
 
 echo "<br /><br />";
 
 			echo "<a href='res.php'><font color='#86af49' size='4'>Go Back</font></a>";
 	
-   $a=$_POST['date'];$c=$_POST['src'];$d=$_POST['des'];
+   $a=$_POST['date'];
+$c=$_POST['src'];
+$d=$_POST['des'];
    
 			if(!empty($_POST['date'])||!empty($_POST['src'])||!empty($_POST['des'])){
    
@@ -66,49 +85,42 @@ echo "<div align='center'><font size='6' color='#86af49'>NO TRAINS !!!! Redirect
 						echo "<br /> <br /> <br />";
 						echo "<th><div align= 'Center'><b>Reservation Inquiry</b></div></th>";
 						
-					    echo "<table style='width:100%' border='1' bgcolor='LightBlue'>";
+echo "<table  border='1' bgcolor='LightBlue'>";
 						
-						echo "<th>PNR</th>";
-						
-						echo "<th>TRAIN NO</th>";
+echo "<th width='5%'>PNR</th><th width='5%'>TRAIN NO</th>
 									
-									echo "<th>TRAIN NAME</th> ";
-											
-											echo "<th>SOURCE</th> ";
-														echo "<th>Destination</th>";
-																echo"<th>DOJ</th>";
-																	echo "<th>TICKETS AC I</th>";
-															echo "<th>AC I TIER(INR)</th>";
-															echo "<th>TICKETS AC II</th>";
-															echo "<th>AC II TIER(INR)</th>";
-													echo "<th>TICKETS AC III</th>";
-											echo "<th>AC III TIER(INR)</th>";
-										echo "</table>";
+<th width='5%'>TRAIN NAME</th><th width='5%'>SOURCE</th><th width='5%'>Destination</th>
+<th width='5%'>DOJ</th><th width='5%'>TICKETS AC I</th><th width='5%'>AC I TIER(INR)</th>
+<th width='5%'>TICKETS AC II</th><th width='5%'>AC II TIER(INR)</th>
+<th width='5%'>TICKETS AC III</th><th width='5%'>AC III TIER(INR)</th>
+<th width='5%'>SLEEPER</th><th width='5%'>SL CLASS(INR)</th>
+</table>";
 										
 									echo "<br /> <br /> <br />";
 									
 					echo "<th><div align= 'Center'>STATUS</div></th>";	
 					
-					echo "<table style='width:100%' border='1' bgcolor= 'LightBlue'>";
+					echo "<table  border='1' bgcolor= 'LightBlue'>";
 						
 						$x=$row['id'];
 								$k=$row['TRAIN_NUMBER'];
 										$src=$row['src'];
 												$des=$row['des'];
 														$date=$row['date'];
-																echo "<th>".$x."</th>";
-																		echo "<th>".$k."</th>";
-										echo "<th>".$row['Tr_name']."</th>";
-													echo "<th>".$src."</th>";
-																echo "<th>".$des."</th>";
-																			echo "<th>".$date."</th>";
-																	echo "<th>".$row['AC_I']."</th>";
-															echo "<th>".$row['FIRST_AC']."</th>";
-											echo "<th>".$row['AC_II']."</th>";
-        						echo "<th>".$row['SEC_AC']."</th>";
-							echo "<th>".$row['AC_III']."</th>";
-						echo "<th>".$row['THIRD_AC']."</th>";
-	  
+echo "<th width='5%'>".$x."</th>";
+echo "<th width='5%'>".$k."</th>";
+echo "<th width='5%'>".$row['Tr_name']."</th>";
+echo "<th width='5%'>".$src."</th>";
+echo "<th width='5%'>".$des."</th>";
+echo "<th width='5%'>".$date."</th>";
+echo "<th width='5%'>".$row['AC_I']."</th>";
+echo "<th width='5%'>".$row['FIRST_AC']."</th>";
+											echo "<th width='5%'>".$row['AC_II']."</th>";
+        						echo "<th width='5%'>".$row['SEC_AC']."</th>";
+							echo "<th width='5%'>".$row['AC_III']."</th>";
+						echo "<th width='5%'>".$row['THIRD_AC']."</th>";
+				echo "<th width='5%'>".$row['SL']."</th>";		
+	  echo "<th width='5%'>".$row['SL_CLASS']."</th>";
 		echo "</tr></table><br><br><br>";
 									echo "<a href='try.php?id=$x'>Book Your Ticket</a>";					
 								}
@@ -123,19 +135,3 @@ echo "<div align='center'><font size='6' color='#86af49'>NO TRAINS !!!! Redirect
 <div class="footer">
   <p><marquee>This is a dummy project. |All Rights Reserved. |@CopyRight 2018-2019</marquee></p>
      </div>					
-					
-					
-					     
-						
-						
-				
-						
-					
-					 
-				
-			
-			
-		
-		
- 
- 
