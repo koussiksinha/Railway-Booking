9662770c91079228c2f717ef6a3eb5c40455089f
Koushik Sinha <38983102+koussiksinha@users.noreply.github.com>
2018-06-12 13:36:01 +0530

    Update pswdupdte.php

--- pswdupdte.php
+++ pswdupdte.php
@@ -1,6 +1,21 @@
 <?php
-include 'timedate.php';
 session_start();
+echo "<font color='#006666'>";
+include 'timedate.php';
+echo "</font>";
+echo "<br>
+<div align='center'>
+<marquee 
+direction='left'
+loop='15'
+scrollamount='2'
+     scrolldelay='2'
+     behavior='alternate'
+     width='70%'
+     	 >
+<font color='#6600FF'>Welcome To&nbsp;&nbsp;E-Ticketing Website</font>
+</marquee>
+</div>";
 ?>
 
 <!DOCTYPE HTML5>
@@ -12,7 +27,16 @@ body {
   font-family: Arial; 
  background-color:MEDIUMAQUAMARINE ;
 }
-</style>
+ .footer {
+	position:fixed;
+   left: 0;
+   bottom:-15;
+   width: 100%;
+   background-color:MEDIUMAQUAMARINE;
+   color:#4B0082;
+   text-align: center;
+}
+ </style>
 </head>
 <body>
 <ul class="navbar">
@@ -28,7 +52,6 @@ body {
  <font color= "Blue">Enter User Name</font>
 <input type ="text" name="uname">
 </p>
-
 <p>
  <font color= "Blue">Enter Old Password</font>
 <input type ="password" name="psw1">
@@ -40,22 +63,28 @@ body {
 <p>
 <font color= "Blue">Re-Type Password</font>
 <input type ="password" name="cpsw">
-
 </p><br/><br/>
-
 <font color= "Blue">SUBMIT</font><input type="submit" name ="submit" value="UPDATE PASSWORD">
 <font color= "Blue">CANCEL</font><input type="RESET" name="reset" value="CANCEL">
 </fieldset>
+<div class="footer">
+  <marquee 
+direction="left"
+loop="15"
+scrollamount="2"
+     scrolldelay='2'
+     behavior="alternate"
+     width="100%"
+     	 >
+<p>This is a dummy project. |All Rights Reserved. |@CopyRight 2018-2019</p>
+</marquee>
+     </div>
 </form>
 </body>
 </head>
 </html>
 
 <!------------------HTML ENDS -------------!>
-<!------------------HTML ENDS -------------!>
-<!------------------HTML ENDS -------------!>
-<!------------------HTML ENDS -------------!>
-
 <?php
 include 'connect1.php';
 @$a=mysqli_real_escape_string($con,$_POST['uname']);
@@ -65,21 +94,15 @@ include 'connect1.php';
 $cx=$_SESSION['id']=$a;
 
 if(isset($_POST['submit']) &&  !empty($_POST['uname']) && !empty($_POST['psw1'])&& !empty($_POST['psw'])&& !empty($_POST['cpsw']))
-{
-	
+{	
 	echo "<br>";
 	echo "<u><font color='Blue' size='6'>Welcome !"." "." ".$_SESSION['id']."</font></u><br><br>";
-	
 	/* FETCH DATA WITH MYSQLI COMMAND. */
 	
 $sql=mysqli_query($con,"select * from test where uname='$a' ");
-
 $row=mysqli_fetch_assoc($sql);
-
 $e=$row['uname'];$f=$row['password'];
-
-if($b==$f ){
-	
+if($b==$f ){	
 if($c==$d){
 	
 	echo "<u>Success</u>"; 
@@ -89,17 +112,14 @@ if($c==$d){
 	
 $u=	mysqli_query($con,"UPDATE test SET password='$c' , cpsw='$d' WHERE uname='$a'");
 echo "<br>";
-
 echo ("<u>Password Change</u>.<br>.<a href='login.php'><u><font color='Blue' size='4'>
 Login Now</font></u></a>"); echo "<br>";
 $s=mysqli_query($con,"select * from test where uname='$a' ");
-
 $row=mysqli_fetch_assoc($s);
 $a=$row['uname'];$f=$row['password'];
 $cx=$row['fname'];$dx=$row['lname'];$ex=$row['email'];
 		
-		/* FETCH DATA WITH MYSQLI COMMAND. */
-		
+	/* FETCH DATA WITH MYSQLI COMMAND. */
 echo "<fieldset>
 <legend><u><font color= 'Brown'>Your Details</font></u></legend>
 <p>
@@ -121,13 +141,8 @@ echo "<fieldset>
  <font color= 'Blue'> New Password</font>
 <input type ='password' value='$f'>
 </p>
-
 </p>
 </fieldset>";
-
-
-
-
 }	
 else{
 	echo "<u><font color='Blue' size='4'> New & Confirm Passwords.Don't Match</font></u>";
@@ -138,28 +153,4 @@ else {
 	echo "<u><font color='Blue' size='4'>Old Password Incorrect!</font></u>";
 }	
 }
-?>	
-	
-	
-	
-	
-
-	
-	
-	
-		
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+?>
